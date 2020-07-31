a3b53b46e75b158ac4c1874dcdfde471c5efa9a7
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-07-31 18:26:55 -0300

    Indentação

--- src/controllers/TradeController.js
+++ src/controllers/TradeController.js
@@ -1,47 +1,54 @@
-const connection = require('../database/connection');
+const connection = require("../database/connection");
 
-const generateUniqueId = require('../utils/generateUniqueId');
+const generateUniqueId = require("../utils/generateUniqueId");
 
 module.exports = {
+  async index(request, response) {
+    const { page = 1 } = request.query;
+    const [count] = await connection("trades").count();
 
-    async index(request, response) {
-        const { page = 1 } = request.query;
-        const [count] = await connection('trades').count();
+    const trades = await connection("trades")
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select("*");
 
+    response.header("X-Total-Count", count["count(*)"]);
 
-        const trades = await connection('trades')
-            .limit(5)
-            .offset((page - 1) * 5).select('*');
+    return response.json(trades);
+  },
 
-        response.header('X-Total-Count', count['count(*)']);
+  async create(request, response) {
+    const {
+      ativo,
+      lado,
+      resultadoOperacao,
+      contratos,
+      quantidadePontos,
+      gatilho,
+      tendencia,
+      regiao,
+      detalhes,
+      data,
+      valorDaOperacao,
+    } = request.body;
 
-        return response.json(trades);
-    },
+    const id = generateUniqueId();
 
-    async create(request, response) {
-        const { type,
-            contracts,
-            hour,
-            trigger,
-            triggerType,
-            localGraph,
-            detail,
-            value } = request.body;
+    await connection("trades").insert({
+      id,
+      ativo,
+      lado,
+      resultadoOperacao,
+      contratos,
+      quantidadePontos,
+      gatilho,
+      tendencia,
+      regiao,
+      detalhes,
+      data,
+      valorDaOperacao,
+    });
 
-        const id = generateUniqueId();
-
-        await connection('trades').insert({
-            id,
-            type,
-            contracts,
-            hour,
-            trigger,
-            triggerType,
-            localGraph,
-            detail,
-            value,
-        })
-
-        return response.json({ id });
-    }
-}
+    return response.json({ id });
+  },
+};
