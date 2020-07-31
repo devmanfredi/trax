f8c23b12484899a000aa701bd8af978d64b52881
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-07-31 19:18:39 -0300

    Correção dos tipos de dados da tabela na migration

--- src/database/migrations/20200331214730_create_trades.js
+++ src/database/migrations/20200331214730_create_trades.js
@@ -1,21 +1,18 @@
-exports.up = function(knex) {
-    return knex.schema.createTable('trades', function (table) {
-        table.string('id').primary();
-        table.int('ativo').notNullable();//INDICES
-        table.int('lado').notNullable();//COMPRA OU VENDA
-        table.string('resultadoOperacao').notNullable();//GAIN/LOSS
-        table.string('contratos').notNullable();//QUANTIDADE DE CONTRATOS
-        table.int('quantidadePontos').notNullable();//PONTOS DA OPERACAO
-        table.string('gatilho').notNullable();//PRICE ACTION/SUPERDOM/REGIAO
-        table.string('tendencia').notNullable();//LTA/LTB
-        table.decimal('regiao').notNullable();//REGIAO DE OPERACAO (102,300)
-        table.decimal('detalhes').notNullable();//DESCRICAO DO POR QUE ENTREI NA OPERACAO
-        table.decimal('data').notNullable();//HORA E DATA DO TRADE
-        table.decimal('valorDaOperacao').notNullable();//VALOR DA OPERACAO
-    });
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("trades", function (table) {
+    table.string("id").primary();
+    table.string("ativo").notNullable(); //INDICES
+    table.int("lado").notNullable(); //COMPRA OU VENDA
+    table.int("resultadoOperacao").notNullable(); //GAIN/LOSS
+    table.int("contratos").notNullable(); //QUANTIDADE DE CONTRATOS
+    table.int("quantidadePontos").notNullable(); //PONTOS DA OPERACAO
+    table.string("gatilho").notNullable(); //PRICE ACTION/SUPERDOM/REGIAO
+    table.int("tendencia").notNullable(); //LTA/LTB
+    table.decimal("regiao").notNullable(); //REGIAO DE OPERACAO (102,300)
+    table.string("detalhes").notNullable(); //DESCRICAO DO POR QUE ENTREI NA OPERACAO
+    table.dateTime("data").notNullable(); //HORA E DATA DO TRADE
+    table.decimal("valorDaOperacao").notNullable(); //VALOR DA OPERACAO
+  });
 };
 
-exports.down = function(knex) {
-    return knex.schema.dropTable('trades');
-};
+exports.down = (knex) => knex.schema.dropTable("trades");
