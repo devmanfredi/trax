717f129347b4d6fa00bfce68aabda2dff2ba99bb
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-07-31 18:27:11 -0300

    nova configuração da tabela

--- src/database/migrations/20200331214730_create_trades.js
+++ src/database/migrations/20200331214730_create_trades.js
@@ -1,14 +1,17 @@
 exports.up = function(knex) {
     return knex.schema.createTable('trades', function (table) {
         table.string('id').primary();
-        table.int('type').notNullable();
-        table.int('contracts').notNullable();
-        table.string('hour').notNullable();
-        table.string('trigger').notNullable();
-        table.int('triggerType').notNullable();
-        table.string('localGraph').notNullable();
-        table.string('detail').notNullable();
-        table.decimal('value').notNullable();
+        table.int('ativo').notNullable();//INDICES
+        table.int('lado').notNullable();//COMPRA OU VENDA
+        table.string('resultadoOperacao').notNullable();//GAIN/LOSS
+        table.string('contratos').notNullable();//QUANTIDADE DE CONTRATOS
+        table.int('quantidadePontos').notNullable();//PONTOS DA OPERACAO
+        table.string('gatilho').notNullable();//PRICE ACTION/SUPERDOM/REGIAO
+        table.string('tendencia').notNullable();//LTA/LTB
+        table.decimal('regiao').notNullable();//REGIAO DE OPERACAO (102,300)
+        table.decimal('detalhes').notNullable();//DESCRICAO DO POR QUE ENTREI NA OPERACAO
+        table.decimal('data').notNullable();//HORA E DATA DO TRADE
+        table.decimal('valorDaOperacao').notNullable();//VALOR DA OPERACAO
     });
   
 };
